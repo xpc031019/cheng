0ef0d5479279587524fe2cc42355377b3b949268
xpc031019 <160452075+xpc031019@users.noreply.github.com>
2024-02-22 11:30:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+app.py
+
 from flask import Flask,request,render_template
 import replicate
 import os
 import time
-from openai import OpenAI
 
-model=OpenAI（api_key=“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")
-
+os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
 
 app = Flask(__name__)
-os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
 
 r = ""
 first_time = 1
@@ -25,6 +24,15 @@ def main():
         first_time=0
     return(render_template("main.html",r=r))
 
+@app.route("/text_gpt",methods=["GET","POST"])
+def text_gpt():
+    return(render_template("text_gpt.html"))
+
+@app.route("/text_result",methods=["GET","POST"])
+def text_result():
+    q = request.form.get("q")
+    return(render_template("text_result.html",r="API not ready"))
+
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
@@ -38,14 +46,14 @@ def image_result():
         "prompt": q,
         }
     )
-    time.sleep(5)
-    return(render_template("text_result.html",r=r[0]))
+    time.sleep(10)
+    return(render_template("image_result.html",r=r[0]))
 
 @app.route("/end",methods=["GET","POST"])
 def end():
-    global first_time
+    global first_time,r
     first_time = 1
-    return(render_template("end.html"))
+    return(render_template("end.html",r=r))
 
 if __name__ == "__main__":
     app.run()
